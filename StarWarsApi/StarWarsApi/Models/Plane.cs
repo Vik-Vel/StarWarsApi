c4155c0856e919b33dd076a04ab92098e43e03e3
Viktoriya Veleva <vikiveleva54@gmail.com>
2024-06-12 18:48:24 +0300

    Plane entity created

---
+++ StarWarsApi/StarWarsApi/Models/Plane.cs
@@ -0,0 +1,18 @@
+﻿namespace StarWarsApi.Models
+{
+    public class Plane
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string RotationPeriod { get; set; } = null!;
+        public string OrbitalPeriod { get; set; } = null!;
+        public string Diameter { get; set; } = null!;
+        public string Climate { get; set; } = null!;
+        public string Gravity { get; set; } = null!;
+        public string Terrain { get; set; } = null!;
+        public string SurfaceWater { get; set; } = null!;
+        public string Population { get; set; } = null!;
+        public string Created { get; set; } = null!;
+        public string Edited { get; set; } = null!;
+    }
+}
